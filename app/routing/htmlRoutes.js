1ebb3c2054c069787b340b6ce1ba840d1d0d6e07
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-05-13 18:46:25 -0500

    little changes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
         res.sendFile(path.join)(__dirname, "../public/survey.html")
     }
 )
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
@@ -8,8 +9,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
